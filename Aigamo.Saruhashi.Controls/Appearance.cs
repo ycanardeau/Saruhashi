534c46b329bfecc30c7fd28336a36424f987e87e
ycanardeau <51428094+ycanardeau@users.noreply.github.com>
2020-07-27 21:51:01 +1000

    Create Appearance

---
+++ Aigamo.Saruhashi.Controls/Appearance.cs
@@ -0,0 +1,24 @@
+﻿// Code from: https://github.com/dotnet/winforms/blob/2cfcc3a6fbb17a4e428bc166fd705c7b41b548a4/src/System.Windows.Forms/src/System/Windows/Forms/Appearance.cs
+
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace Aigamo.Saruhashi
+{
+	/// <summary>
+	///  Specifies the appearance of a control.
+	/// </summary>
+	public enum Appearance
+	{
+		/// <summary>
+		///  The default appearance defined by the control class.
+		/// </summary>
+		Normal = 0,
+
+		/// <summary>
+		///  The appearance of a Windows button.
+		/// </summary>
+		Button = 1,
+	}
+}
