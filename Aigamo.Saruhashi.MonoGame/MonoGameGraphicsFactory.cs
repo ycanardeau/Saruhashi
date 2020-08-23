91e0920429bcd983f32d4bbf1c4c890fb1226ec0
Aigamo <51428094+ycanardeau@users.noreply.github.com>
2020-08-23 20:07:58 +1000

    Restore Aigamo.Saruhashi.MonoGame

---
+++ Aigamo.Saruhashi.MonoGame/MonoGameGraphicsFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+
+namespace Aigamo.Saruhashi.MonoGame
+{
+	public sealed class MonoGameGraphicsFactory : IGraphicsFactory
+	{
+		public MonoGameGraphicsFactory(SpriteBatch spriteBatch)
+		{
+			SpriteBatch = spriteBatch;
+		}
+
+		public SpriteBatch SpriteBatch { get; }
+
+		public Graphics Create(Control control) => new MonoGameGraphics(control, SpriteBatch);
+	}
+}
---
+++ Aigamo.Saruhashi.MonoGame/MonoGameImage.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Aigamo.Saruhashi.MonoGame
+{
+	public sealed class MonoGameImage : IImage
+	{
+		public MonoGameImage(Texture2D texture, Color color)
+		{
+			Texture = texture;
+			Color = color;
+		}
+
+		public Texture2D Texture { get; }
+		public Color Color { get; }
+
+	}
+}
