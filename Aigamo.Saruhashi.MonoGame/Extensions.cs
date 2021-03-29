7170b61b765cf8d81f74f2280d3cbfdbb92a7067
Aigamo <51428094+ycanardeau@users.noreply.github.com>
2021-03-29 17:52:24 +1000

    Use target-typed new

--- Aigamo.Saruhashi.MonoGame/Extensions.cs
+++ Aigamo.Saruhashi.MonoGame/Extensions.cs
@@ -12,13 +12,13 @@ namespace Aigamo.Saruhashi.MonoGame
 	public static class Extensions
 	{
 		public static DrawingColor ToDrawingColor(this XnaColor color) => DrawingColor.FromArgb(color.A, color.R, color.G, color.B);
-		public static DrawingPoint ToDrawingPoint(this XnaPoint point) => new DrawingPoint(point.X, point.Y);
-		public static DrawingRectangle ToDrawingRectangle(this XnaRectangle rectangle) => new DrawingRectangle(rectangle.X, rectangle.Y, rectangle.Width, rectangle.Height);
-		public static DrawingSizeF ToDrawingSize(this XnaSize2 size) => new DrawingSizeF(size.Width, size.Height);
+		public static DrawingPoint ToDrawingPoint(this XnaPoint point) => new(point.X, point.Y);
+		public static DrawingRectangle ToDrawingRectangle(this XnaRectangle rectangle) => new(rectangle.X, rectangle.Y, rectangle.Width, rectangle.Height);
+		public static DrawingSizeF ToDrawingSize(this XnaSize2 size) => new(size.Width, size.Height);
 
-		public static XnaColor ToXnaColor(this DrawingColor color) => new XnaColor(color.R, color.G, color.B, color.A);
-		public static XnaPoint ToXnaPoint(this DrawingPoint point) => new XnaPoint(point.X, point.Y);
-		public static XnaRectangle ToXnaRectangle(this DrawingRectangle rectangle) => new XnaRectangle(rectangle.X, rectangle.Y, rectangle.Width, rectangle.Height);
-		public static XnaSize2 ToXnaSize(this DrawingSizeF size) => new XnaSize2(size.Width, size.Height);
+		public static XnaColor ToXnaColor(this DrawingColor color) => new(color.R, color.G, color.B, color.A);
+		public static XnaPoint ToXnaPoint(this DrawingPoint point) => new(point.X, point.Y);
+		public static XnaRectangle ToXnaRectangle(this DrawingRectangle rectangle) => new(rectangle.X, rectangle.Y, rectangle.Width, rectangle.Height);
+		public static XnaSize2 ToXnaSize(this DrawingSizeF size) => new(size.Width, size.Height);
 	}
 }
